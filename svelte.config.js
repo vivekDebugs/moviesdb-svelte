f11c118b29a2bff5842127a904d5cd04a9ea8b4c
Vivek Kumar <vivekkumar.1644@gmail.com>
2022-01-25 23:17:30 +0530

    changed to esmodule import

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,6 @@
 // import adapter from '@sveltejs/adapter-auto';
-const netlify = require('@sveltejs/adapter-netlify');
+// const netlify = require('@sveltejs/adapter-netlify');
+import netlify from '@sveltejs/adapter-netlify';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
